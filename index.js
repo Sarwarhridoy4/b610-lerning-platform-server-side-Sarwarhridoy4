ff948ddff80612b5393232df906c2044a4e6e82c
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-10-27 08:21:48 +0600

    inlex file updated

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(cors());
 
 const categories = require("./Data/Category/categories.json");
 const allcourse = require("./Data/All Course/All Course.json");
+const blogs = require("./Data/Blogs/Blog.json");
 
 app.get("/", (req, res) => {
   res.send("E- Pathshala server running...");
@@ -20,6 +21,10 @@ app.get("/course-categories", (req, res) => {
 
 app.get("/courses", (req, res) => {
   res.send(allcourse);
+});
+
+app.get("/blogs", (req, res) => {
+  res.send(blogs);
 });
 
 app.get("/category/:id", (req, res) => {
